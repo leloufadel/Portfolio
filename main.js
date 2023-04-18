b78e59c12359a71a9c774e02db7807603b50905e
jubaer919 <60255197+jubaer919@users.noreply.github.com>
2023-04-18 14:35:39 +0600

    add some functionality in js file and make some change in html and css accordingly

--- main.js
+++ main.js
@@ -21,10 +21,47 @@
 
 //  });
 const hamburger = document.querySelector(".hamberger");
-const mynav = document.querySelector(".nav-menu");
+// const mynav = document.querySelector(".nav-menu");
+const header = document.querySelector("header");
 
 
-hamburger.addEventListener("click", () => {
-    hamburger.classList.toggle("active");
-    mynav.classList.toggle("active");
-} );
+// hamburger.addEventListener("click", () => {
+//     header.classList.toggle("active");
+//     // mynav.classList.toggle("active");
+// } );
+
+// const header = document.querySelector('.header');
+
+const body = document.querySelector('body');
+
+// Set the screen width threshold
+const MOBILE_WIDTH = 768;
+
+function disableScroll() {
+  body.style.overflow = 'hidden';
+}
+
+function enableScroll() {
+  body.style.overflow = 'auto';
+}
+
+// Function to add or remove the "mobile" class based on the screen width
+function setMobileClass() {
+  if (window.innerWidth < MOBILE_WIDTH) {
+    header.addEventListener('click', () => {
+      header.classList.toggle('active');
+      // Toggle scroll
+      if (header.classList.contains('active')) {
+        disableScroll();
+      } else {
+        enableScroll();
+      }
+    });
+  }
+}
+// Call the function on page load
+setMobileClass();
+
+// Call the function on window resize
+window.addEventListener('resize', setMobileClass);
+
