13f877a1876137eeec765497086f874dc680342d
leloufadel <leloufadel@gmail.com>
2023-04-18 13:01:14 +0300

    fixed linters

--- main.js
+++ main.js
@@ -1,18 +1,13 @@
 const header = document.querySelector('header');
-
 const body = document.querySelector('body');
-
 // Set the screen width threshold
 const MOBILE_WIDTH = 768;
-
 function disableScroll() {
   body.style.overflow = 'hidden';
 }
-
 function enableScroll() {
   body.style.overflow = 'auto';
 }
-
 // Function to add or remove the "mobile" class based on the screen width
 function setMobileClass() {
   if (window.innerWidth < MOBILE_WIDTH) {
@@ -29,6 +24,5 @@ function setMobileClass() {
 }
 // Call the function on page load
 setMobileClass();
-
 // Call the function on window resize
 window.addEventListener('resize', setMobileClass);
