0830093b2f59becd27c81d9ffd4275068925fd63
leloufadel <leloufadel@gmail.com>
2023-04-18 22:44:01 +0300

    update CSS and JS files

--- main.js
+++ main.js
@@ -1,16 +1,20 @@
 const header = document.querySelector('header');
+const humberger = document.querySelector('.hamberger');
 const body = document.querySelector('body');
 const MOBILE_WIDTH = 768;
 function disableScroll() {
   body.style.overflow = 'hidden';
+ 
 }
 function enableScroll() {
   body.style.overflow = 'auto';
+  
 }
 function setMobileClass() {
   if (window.innerWidth < MOBILE_WIDTH) {
-    header.addEventListener('click', () => {
+    humberger.addEventListener('click', () => {
       header.classList.toggle('active');
+      
       if (header.classList.contains('active')) {
         disableScroll();
       } else {
