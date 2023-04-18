606f0cc994f2ced91aa75f2959ce578111b49914
leloufadel <leloufadel@gmail.com>
2023-04-18 23:23:58 +0300

    Fix linters

--- main.js
+++ main.js
@@ -4,17 +4,15 @@ const body = document.querySelector('body');
 const MOBILE_WIDTH = 768;
 function disableScroll() {
   body.style.overflow = 'hidden';
- 
 }
 function enableScroll() {
   body.style.overflow = 'auto';
-  
 }
 function setMobileClass() {
   if (window.innerWidth < MOBILE_WIDTH) {
     humberger.addEventListener('click', () => {
       header.classList.toggle('active');
-      
+
       if (header.classList.contains('active')) {
         disableScroll();
       } else {
