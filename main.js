73a16a62196cfdbc09f9de8ac6f37495d867c7ea
leloufadel <leloufadel@gmail.com>
2023-04-18 12:58:17 +0300

    fixed linters

--- main.js
+++ main.js
@@ -1,35 +1,4 @@
-// const hamburgerButton = document.getElementById('hamburger-button');
-// const mobileMenu = document.querySelector('.menu');
-
-// hamburgerButton.addEventListener('click', () => {
-//   mobileMenu.classList.toggle('show');
-// });
-// const menuItems = mobileMenu.querySelectorAll('a');
-// menuItems.forEach(item => {
-//   item.addEventListener('click', () => {
-//     mobileMenu.classList.remove('show');
-//   });
-// });
-
-// const humberger = document.querySelector(".humberger");
-
-// humberger.addEventListener("click", () => {
-
-// const navigations = document.querySelector(".navigations");
-
-// navigations.style.display = 'flex';
-
-//  });
-// const hamburger = document.querySelector('.hamberger');
-// const mynav = document.querySelector(".nav-menu");
 const header = document.querySelector('header');
-
-// hamburger.addEventListener("click", () => {
-//     header.classList.toggle("active");
-//     // mynav.classList.toggle("active");
-// } );
-
-// const header = document.querySelector('.header');
 
 const body = document.querySelector('body');
 
